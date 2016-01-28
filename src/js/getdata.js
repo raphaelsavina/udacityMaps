f15a8befbcaf4de837a18aebaeb7856d4eaacff9
Raphael Savina <raphael@savina.net>
2016-01-28 08:23:28 +0000

    js to get data from firebase

--- src/js/getdata.js
+++ src/js/getdata.js
@@ -1,10 +1,39 @@
 // Connection to firebase
 var FirebaseRef = new Firebase("https://udacitymapping.firebaseio.com/");
 
+var getLocationfromDatabase = function(callback) {
+    FirebaseRef.once("value", function(data) {
+        if (data.exists()) {
+            success(data.val(), callback);
+        }
+    }, function(error) {
+        failed(error);
+    });
+}
+
+var success = function(data, callback) {
+    for (l in data) {
+        locations.push({
+          id: l,
+          title: data[l].title,
+          description: data[l].description,
+          like: data[l].likes,
+          lat: data[l].lat,
+          lng: data[l].lng,
+          url: data[l].url
+        });
+    };
+    callback();
+}
+
+var failed = function(error) {
+    console.log(error.code);
+}
+
 var addLocationtoDatabase = function(title, description, lat, lng, url) {
     var newLocation = FirebaseRef.push({
-    	lat: lat,
-    	lng: lng,
+        lat: lat,
+        lng: lng,
         title: title,
         description: description,
         url: url,
